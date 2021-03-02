cde89e586070f64d225126ee33e006fe267ee423
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-03-02 18:58:36 +0530

    commit with basic functionality of tkinter and turtle GUI of Python.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import tkinter
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
 
+# Label
+my_label = tkinter.Label(text="This Is Label...", font=("Arial", 24, "bold", "italic"))
+my_label.pack(expand=True)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import turtle
 
+tim = turtle.Turtle()
+tim.shape("turtle")
+tim.write()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+window.mainloop()
